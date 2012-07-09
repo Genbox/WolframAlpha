998a7c592705dd5ed194ad3cad86ad159a2124e8
Ian Qvist <qvistian@gmail.com>
2012-07-09 19:26:55 +0200

    Implemented equality in QueryResults

--- WolframAlphaNET/Objects/QueryResult.cs
+++ WolframAlphaNET/Objects/QueryResult.cs
@@ -63,5 +63,24 @@ namespace WolframAlphaNET.Objects
         /// </summary>
         public MathML MathML { get; set; }
 
+        public bool Equals(QueryResult other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(other.ID, ID);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof(QueryResult)) return false;
+            return Equals((QueryResult)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (ID != null ? ID.GetHashCode() : 0);
+        }
     }
 }
